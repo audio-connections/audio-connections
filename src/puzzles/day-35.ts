53d44c746269d8080f2dc198550cfb68eee4c8c6
Frankie <>
2026-05-27 22:22:27 -0700

    Incorporated feedback

--- src/puzzles/day-35.ts
+++ src/puzzles/day-35.ts
@@ -7,7 +7,7 @@ const puzzle: Puzzle = {
   releaseAt: '2026-06-14T00:00:00Z',
   themes: [
     {
-      theme: 'Songs titles that reference the calendar.',
+      theme: 'Song titles that reference the calendar.',
       tracks: [
         { id: 1771709877, artist: 'Phoenix', title: '1901', note: 'The Belle Époque Era' },
         { id: 1440839338, artist: 'Sublime', title: 'April 29th, 1992 (Miami)', note: 'LA Riots' },
@@ -20,12 +20,12 @@ const puzzle: Puzzle = {
       tracks: [
         { id: 306676853, artist: 'DJ Jazzy Jeff & the Fresh Prince', title: "Parents just don't understand", note: '' },
         { id: 1649315667, artist: 'Black Lips', title: 'Bad Kids', note: '' },
-        { id: 1440783625, artist: 'Nirvana', title: 'Smells Like Teen Spirit', note: '' },
+        { id: 1440912353, artist: 'Beastie Boys', title: "Fight for Your Right", note: '' },
         { id: 209388998, artist: 'My Chemical Romance', title: 'Teenagers', note: '' },
       ],
     },
     {
-      theme: 'Songs sang by children',
+      theme: 'Songs sung by children',
       tracks: [
         { id: 1446924089, artist: 'Musical Youth', title: 'Pass the Dutchie', note: '11 to 15' },
         { id: 170147030, artist: 'Kriss Kross', title: 'Jump', note: '12 and 13' },
@@ -34,7 +34,7 @@ const puzzle: Puzzle = {
       ],
     },
         {
-      theme: 'VMA song of the year winners',
+      theme: 'VMA video of the year winners',
       tracks: [
         { id: 270246724, artist: 'TLC', title: 'Waterfalls', note: '' },
         { id: 1476727670, artist: 'Lady Gaga', title: 'Bad Romance', note: '' },

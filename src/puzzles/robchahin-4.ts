20db7580166aa58895a0fcf67a714136521d9aee
Rob Chahin <github@robchahin.com>
2026-06-14 22:30:51 -0700

    Add Rob Chahin color puzzle

---
+++ src/puzzles/robchahin-4.ts
@@ -0,0 +1,45 @@
+import type { PuzzleContent } from '../types';
+
+const puzzle: PuzzleContent = {
+  author: 'Rob Chahin',
+  themes: [
+    {
+      theme: 'Purple',
+      tracks: [
+        { id: 357225355, artist: 'The Jimi Hendrix Experience', title: 'Purple Haze' },
+        { id: 1443175647, artist: 'D12', title: 'Purple Hills' },
+        { id: 1125270043, artist: 'Skrillex & Rick Ross', title: 'Purple Lamborghini' },
+        { id: 1099335223, artist: 'Deep Purple', title: 'Smoke On the Water' },
+      ],
+    },
+    {
+      theme: 'Blue',
+      tracks: [
+        { id: 1396312106, artist: 'Fats Domino', title: 'Blueberry Hill' },
+        { id: 217633914, artist: 'Elvis Presley', title: 'Blue Suede Shoes' },
+        { id: 1039656217, artist: 'David Bowie', title: 'The Jean Genie' },
+        { id: 1500824680, artist: 'Miranda Lambert', title: 'Bluebird' },
+      ],
+    },
+    {
+      theme: 'Yellow',
+      tracks: [
+        { id: 1445949267, artist: 'Post Malone & Swae Lee', title: 'Sunflower' },
+        { id: 1598660389, artist: 'BTS', title: 'Butter' },
+        { id: 1440857795, artist: 'Jack Johnson', title: 'Banana Pancakes' },
+        { id: 723501674, artist: 'Katrina and the Waves', title: 'Walking On Sunshine' },
+      ],
+    },
+    {
+      theme: 'Green',
+      tracks: [
+        { id: 1440820532, artist: 'ABBA', title: 'Money, Money, Money' },
+        { id: 1547316718, artist: 'Taylor Swift', title: 'ivy' },
+        { id: 785239189, artist: 'Black Sabbath', title: 'Sweet Leaf' },
+        { id: 304815015, artist: 'Harry Nilsson', title: 'Coconut' },
+      ],
+    },
+  ],
+};
+
+export default puzzle;

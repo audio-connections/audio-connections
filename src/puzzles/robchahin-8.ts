fd840f6603b9f746927a82a3e9852b8bf096aa24
Rob Chahin <github@robchahin.com>
2026-07-21 20:36:43 -0700

    Add puzzle submission

---
+++ src/puzzles/robchahin-8.ts
@@ -0,0 +1,45 @@
+import type { PuzzleContent } from '../types';
+
+const puzzle: PuzzleContent = {
+  author: 'Rob Chahin',
+  themes: [
+    {
+      theme: 'Colors in the song title',
+      tracks: [
+        { id: 1441133834, artist: 'The Beatles', title: 'Blackbird', note: '' },
+        { id: 1440745782, artist: 'The Rolling Stones', title: 'Paint It, Black', note: '' },
+        { id: 1533513661, artist: 'The White Stripes', title: 'Blue Orchid', note: '' },
+        { id: 1440814573, artist: 'The Velvet Underground', title: 'Pale Blue Eyes', note: '' },
+      ],
+    },
+    {
+      theme: 'Famous cover songs',
+      tracks: [
+        { id: 647246433, artist: 'The Clash', title: 'I Fought the Law', note: 'The Crickets' },
+        { id: 200008782, artist: 'The Bangles', title: 'Hazy Shade of Winter', note: 'Simon & Garfunkel' },
+        { id: 153421332, artist: 'The Byrds', title: 'Mr. Tambourine Man', note: 'Bob Dylan' },
+        { id: 1440733884, artist: 'The Rolling Stones', title: 'Not Fade Away', note: 'Buddy Holly & The Crickets' },
+      ],
+    },
+    {
+      theme: 'Prominent falsetto vocals',
+      tracks: [
+        { id: 1442259185, artist: 'Bee Gees', title: "Stayin' Alive", note: '' },
+        { id: 6433514, artist: 'The Darkness', title: 'I Believe in a Thing Called Love', note: '' },
+        { id: 1442865074, artist: 'The Beach Boys', title: 'Good Vibrations', note: '' },
+        { id: 1440812173, artist: 'The Rolling Stones', title: 'Emotional Rescue', note: '' },
+      ],
+    },
+    {
+      theme: 'Titles never said in the lyrics',
+      tracks: [
+        { id: 1440850320, artist: 'The Who', title: "Baba O'Riley", note: '' },
+        { id: 1450141548, artist: 'The Cardigans', title: 'Lovefool', note: '' },
+        { id: 721207643, artist: 'The Smashing Pumpkins', title: 'Cherub Rock', note: '' },
+        { id: 1500643398, artist: 'The Rolling Stones', title: 'Sympathy for the Devil', note: '' },
+      ],
+    },
+  ],
+};
+
+export default puzzle;

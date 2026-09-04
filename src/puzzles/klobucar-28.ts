ce0034e315e5b4bdb0e93a2757dce56db96ad8b1
Jonathon Klobucar <jonathon.klobucar@gmail.com>
2026-09-03 18:10:50 -0700

    chore(puzzle): add klobucar-28 puzzle to backlog

---
+++ src/puzzles/klobucar-28.ts
@@ -0,0 +1,45 @@
+import type { PuzzleContent } from '../types';
+
+const puzzle: PuzzleContent = {
+  author: 'Jonathon Klobucar',
+  themes: [
+    {
+      theme: 'Flowers or flowering plants in the song title',
+      tracks: [
+        { id: 1815942907, artist: 'The Foundations', title: 'Build Me Up Buttercup' },
+        { id: 716552556, artist: 'Poison', title: 'Every Rose Has Its Thorn' },
+        { id: 1445732605, artist: 'Hole', title: 'Violet' },
+        { id: 140785376, artist: 'Tiny Tim', title: 'Tip Toe Thru\' the Tulips With Me' },
+      ],
+    },
+    {
+      theme: 'Songs featuring an iconic spoken phone call or telephone ringing',
+      tracks: [
+        { id: 541046802, artist: 'The Big Bopper', title: 'Chantilly Lace', note: 'Opens with telephone ringing and "Hello, baby! Yeah, this is the Big Bopper speakin\'!"' },
+        { id: 196428500, artist: 'Electric Light Orchestra', title: 'Telephone Line', note: 'Features authentic telephone ringtone and operator tone' },
+        { id: 1443711628, artist: 'Soulja Boy Tell \'Em', title: 'Kiss Me Thru the Phone', note: 'Features phone ringing audio effect and keypad dialing tones' },
+        { id: 1476727675, artist: 'Lady Gaga', title: 'Telephone (feat. Beyoncé)', note: 'Includes telephone ringing and operator voicemail announcement' },
+      ],
+    },
+    {
+      theme: 'Songs featured on the Guardians of the Galaxy: Awesome Mix Vol. 1',
+      tracks: [
+        { id: 197055097, artist: 'Redbone', title: 'Come and Get Your Love' },
+        { id: 1440747928, artist: 'The Runaways', title: 'Cherry Bomb' },
+        { id: 1440924549, artist: '10cc', title: 'I\'m Not In Love' },
+        { id: 1443816839, artist: 'Elvin Bishop', title: 'Fooled Around and Fell in Love' },
+      ],
+    },
+    {
+      theme: 'Musicians who had long-running acting roles as series regulars on prime-time television',
+      tracks: [
+        { id: 321974628, artist: 'Brandy', title: 'I Wanna Be Down', note: 'Starred as Moesha Mitchell on Moesha (127 episodes)' },
+        { id: 921505748, artist: 'Ice T', title: 'Colors', note: 'Starred as Detective Odafin Tutuola on Law & Order: SVU (500+ episodes)' },
+        { id: 1443011616, artist: 'LL COOL J', title: 'Mama Said Knock You Out', note: 'Starred as Special Agent Sam Hanna on NCIS: Los Angeles (300+ episodes)' },
+        { id: 1523081558, artist: 'Reba McEntire', title: 'Fancy', note: 'Starred as Reba Hart on sitcom Reba (127 episodes)' },
+      ],
+    },
+  ],
+};
+
+export default puzzle;
